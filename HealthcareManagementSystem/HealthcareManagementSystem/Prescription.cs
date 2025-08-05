3be7b173061e7008103b27d64243cf884af1aa9f
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-08-05 14:18:03 +0000

    feat: Add Prescription class with fields and constructor
    - Defined Id, PatientId, MedicationName, and DateIssued properties
    
    - Implemented constructor to initialize all fields
    
    - Supports linking prescriptions to patient records

---
+++ HealthcareManagementSystem/HealthcareManagementSystem/Prescription.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthcareManagementSystem
+{
+    public class Prescription
+    {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public string MedicationName { get; set; }
+        public DateTime DateIssued { get; set; }
+
+        // Constructor to initialize a new prescription
+        public Prescription(int id, int patientId, string medicationName, DateTime dateIssued)
+        {
+            Id = id;
+            PatientId = patientId;
+            MedicationName = medicationName;
+            DateIssued = dateIssued;
+        }
+    }
+}
