f8948879e0387226e33085673faff6b5bc3fe3bc
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-08-05 15:01:54 +0000

    feat: Add prescription management using Dictionary<int, List<Prescription>>
    - Implemented AddPrescription method to store prescriptions by patient ID
    
    - Created GetPrescriptionsByPatientId method to retrieve prescriptions
    
    - Used Dictionary to efficiently group and access prescriptions per patient
    
    - Ensures fallback to empty list if no prescriptions found

---
+++ HealthcareManagementSystem/HealthcareManagementSystem/Program2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HealthcareManagementSystem
+{
+    class Program2
+    {
+        // Retrieve the list of prescriptions for the specified patient ID.
+        // If patient ID is not found, return an empty list.
+        public List<Prescription> GetPrescriptionsByPatientId(int PatientID)
+        {
+            if (patientPrescription.TryGetValue(PatientID, out var prescriptions))
+            {
+                return prescriptions;
+            }
+            else
+            {
+                return new List<Prescription>();
+            }
+        }
+
+        // Dictionary to store prescriptions for each patient.
+        // Key: Patient ID, Value: List of prescriptions issued to that patient.
+        private Dictionary<int, List<Prescription>> patientPrescription = new Dictionary<int, List<Prescription>>();
+
+        public void AddPrescription(Prescription prescription)
+        {
+            if (patientPrescription.ContainsKey(prescription.PatientId))
+            {
+                patientPrescription[prescription.PatientId].Add(prescription);
+            }
+            else
+            {
+                patientPrescription[prescription.PatientId] = new List<Prescription>() { prescription };
+            }
+        }
+    }
+}
+
